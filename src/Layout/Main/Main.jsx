16a6df561d17123d73b53af04321e8ec2ab6386a
Taslim <taslimahmed313@gmil.com>
2023-04-09 15:49:33 +0600

    Navbar Added

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../../Pages/Shared/Footer';
-import Navbar from '../../Pages/Shared/Navbar';
+import Navbar from '../../Pages/Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
